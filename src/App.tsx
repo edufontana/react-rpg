7b814b92755fec1e8e0e8d5fffcc5c697192e86f
edu <edufontana7@gmail.com>
2022-10-15 16:01:32 -0300

    feat: character movements

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
+import { useEffect } from 'react'
+
 import { Container, Map } from './App.styles'
 import { GlobalStyle } from './styles/global'
 import { Character } from './components/Character'
+import { useCharacter } from './hooks/useCharacter'
 
 export function App() {
+  const { moveDown, moveLeft, moveRight, moveUp, x, y } = useCharacter()
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+  }, [])
+
+  function handleKeyDown(e: KeyboardEvent) {
+    switch (e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        moveLeft()
+        break
+      case 'KeyW':
+      case 'ArrowUp':
+        moveUp()
+        break
+      case 'KeyD':
+      case 'ArrowRight':
+        moveRight()
+        break
+      case 'KeyS':
+      case 'ArrowDown':
+        moveDown()
+        break
+    }
+  }
+
   return (
     <>
       <Container>
         <Map>
-          <Character x={5} y={5} />
+          <Character x={x} y={y} />
         </Map>
       </Container>
       <GlobalStyle />
