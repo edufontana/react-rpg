9bb4cfe16cd54739402ee7ce569ce3166be3b863
edu <edufontana7@gmail.com>
2022-10-17 01:17:32 -0300

    feat: map limit

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { mapSpots } from '../data/mapSpots'
 
 export const useCharacter = () => {
   const [pos, setPos] = useState({ x: 3, y: 5 })
@@ -6,7 +7,7 @@ export const useCharacter = () => {
 
   const moveLeft = () => {
     setPos((pos) => ({
-      x: pos.x - 1,
+      x: canMove(pos.x - 1, pos.y) ? pos.x - 1 : pos.x,
       y: pos.y,
     }))
 
@@ -15,7 +16,7 @@ export const useCharacter = () => {
 
   const moveRight = () => {
     setPos((pos) => ({
-      x: pos.x + 1,
+      x: canMove(pos.x + 1, pos.y) ? pos.x + 1 : pos.x,
       y: pos.y,
     }))
     setCharPosition(-60)
@@ -24,7 +25,7 @@ export const useCharacter = () => {
   const moveDown = () => {
     setPos((pos) => ({
       x: pos.x,
-      y: pos.y + 1,
+      y: canMove(pos.x, pos.y + 1) ? pos.y + 1 : pos.y,
     }))
 
     setCharPosition(0)
@@ -33,10 +34,20 @@ export const useCharacter = () => {
   const moveUp = () => {
     setPos((pos) => ({
       x: pos.x,
-      y: pos.y - 1,
+      y: canMove(pos.x, pos.y - 1) ? pos.y - 1 : pos.y,
     }))
 
     setCharPosition(-90)
+  }
+
+  const canMove = (x: number, y: number) => {
+    if (mapSpots[y] !== undefined && mapSpots[y][x] !== undefined) {
+      if (mapSpots[y][x] === 1) {
+        return true
+      }
+    }
+
+    return false
   }
 
   return {
